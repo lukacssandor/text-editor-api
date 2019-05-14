8dfba3fe81a6c95a74793b94eac090e356055c31
Lukács Sándor <lukacssandor98@gmail.com>
2019-05-14 11:18:11 +0200

    server

--- server.js
+++ server.js
@@ -66,6 +66,6 @@ app.post("/savetodrive", async(req, res1, next) => {
 	});
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
 	console.log("Server running on port 3000");
 });
